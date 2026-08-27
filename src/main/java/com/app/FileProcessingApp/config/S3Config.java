c246fcfa29dc6d24d2b3ee257ac915be4a4d556a
Sumit <sumitkherkar169@gmail.com>
2026-08-27 10:41:39 +0530

    Some local changes

--- src/main/java/com/app/FileProcessingApp/config/S3Config.java
+++ src/main/java/com/app/FileProcessingApp/config/S3Config.java
@@ -3,6 +3,7 @@ package com.app.FileProcessingApp.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
@@ -12,6 +13,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
+@Profile("dev")
 public class S3Config {
 
      @Value("${aws.s3.access_key}")
